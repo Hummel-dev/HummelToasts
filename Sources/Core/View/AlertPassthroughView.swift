e2dab8ace6d5270b0600b634c0a5eb60411e81f8
Archibbald <88779697+ArchiProger@users.noreply.github.com>
2025-03-05 13:07:03 +0300

    Drag gesture optimization

--- Sources/Core/View/AlertPassthroughView.swift
+++ Sources/Core/View/AlertPassthroughView.swift
@@ -13,7 +13,7 @@ import Toasts
 struct AlertPassthroughView: View {
     typealias Notification = ToastsFoundation.Notification
     
-    @GestureState var offset: CGFloat = .zero
+    @State var offset: CGFloat = .zero
     
     @ObservedObject var notificationVM = NotificationViewModel()
     
@@ -25,7 +25,6 @@ struct AlertPassthroughView: View {
                 .id(toast.id)
                 .offset(y: offset)
                 .gesture(toast.isControlGesturesActive ? closingGesture(toast: toast) : nil)
-                .animation(.default.speed(1.5), value: offset)
                 .frame(
                     maxHeight: .infinity,
                     alignment: Alignment(horizontal: .center, vertical: toast.alignment)
@@ -40,19 +39,20 @@ struct AlertPassthroughView: View {
                         }
                      )
                 )
+                .onAppear {
+                    offset = .zero
+                }
         }
     }
     
     func closingGesture(toast: any Toast) -> some Gesture {
-        DragGesture()
-            .updating($offset) { value, state, transaction in
+        DragGesture(minimumDistance: 20)
+            .onChanged { value in
                 if value.location.y < 0 {
-                    state = value.location.y
+                    offset = value.location.y
                 }
             }
             .onEnded { value in
-                guard value.translation.height < 20 else { return }
-                
                 withAnimation(toast.animation) {
                     notificationVM.notification = nil
                 }
